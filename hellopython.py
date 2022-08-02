2c8a23a89515b6717523cf16bb4921d7f49bc62d
HieuHTHE151387 <nanhtrang013@gmail.com>
2022-08-02 09:02:35 +0700

    Hi py

---
+++ hellopython.py
@@ -0,0 +1,10 @@
+# first_name = input("Your first name is: ")
+# last_name = input("Your last name is: ")
+
+# def new_func():
+#     print("Hello world,I\'m "+first_name+" "+last_name)
+
+# new_func()  
+
+print("Hello world!")
+
